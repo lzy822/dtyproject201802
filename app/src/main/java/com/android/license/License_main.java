b4122780a82f45e8313da7398ba75007526f78c4
Zhengyang <542865759@qq.com>
2018-06-11 16:33:09 +0800

    更新授权码算法

--- app/src/main/java/com/android/license/License_main.java
+++ app/src/main/java/com/android/license/License_main.java
@@ -57,6 +57,32 @@ public class License_main extends AppCompatActivity {
         return password;
     }
 
+    //计算识别码
+    private String getPassword1(String deviceId){
+        //String password;
+        //password = "l" + encryption(deviceId) + "ZY";
+        SimpleDateFormat df = new SimpleDateFormat(MyApplication.getContext().getResources().getText(R.string.Date_1).toString());
+        Date nowDate = new Date(System.currentTimeMillis());
+        return encryption(deviceId) + encryption1(df.format(nowDate));
+    }
+
+    private String getDeltaDate(String password){
+        if (deltaTime == 7){
+            password = password + "x";
+        }else if (deltaTime == 180){
+            password = password + "X";
+        }else if (deltaTime == 366){
+            password = password + "y";
+        }else if (deltaTime == 30){
+            password = password + "g";
+        }else if (deltaTime == 90){
+            password = password + "G";
+        }else if (deltaTime == 3660){
+            password = password + "j";
+        }
+        return password;
+    }
+
     //编码
     private String encryption(String password){
         password = password.replace("0", "q");
@@ -75,6 +101,47 @@ public class License_main extends AppCompatActivity {
         password = password.replace("D", "n");
         password = password.replace("E", "v");
         password = password.replace("F", "C");
+        return password;
+    }
+
+    //编码
+    private String encryption1(String password){
+        password = password.replace("0", "a");
+        password = password.replace("1", "Z");
+        password = password.replace("2", "o");
+        password = password.replace("3", "M");
+        password = password.replace("4", "e");
+        password = password.replace("5", "w");
+        password = password.replace("6", "z");
+        password = password.replace("7", "R");
+        password = password.replace("8", "D");
+        password = password.replace("9", "q");
+        return password;
+    }
+
+    //反转授权码
+    private String reverseStr(String str){
+        return str.substring(6, 10) + str.substring(0, 6) + str.substring(10);
+    }
+
+    //恢复反转授权码
+    private String reReverseStr(String str){
+        return str.substring(4,10) + str.substring(0, 4) + str.substring(10);
+    }
+
+    //日期提取算法
+    private String getDateFromStr(String password){
+        password = password.replace("a", "0");
+        password = password.replace("Z", "1");
+        password = password.replace("o", "2");
+        password = password.replace("M", "3");
+        password = password.replace("e", "4");
+        password = password.replace("w", "5");
+        password = password.replace("z", "6");
+        password = password.replace("R", "7");
+        password = password.replace("D", "8");
+        password = password.replace("q", "9");
+        password = password.substring(0, 4) + "年" + password.substring(4, 6) + "月" + password.substring(6) + "日";
         return password;
     }
 
@@ -141,7 +208,7 @@ public class License_main extends AppCompatActivity {
                                 }
                             }
                             if (!isExist){
-                                password = getPassword(str);
+                                password = getDeltaDate(reverseStr(getPassword1(str.substring(str.length() - 6))));
                                 textView.setText("授权码为: " + password + "(长按复制)");
                                 licenses license = new licenses();
                                 license.setImei(str);
@@ -177,7 +244,7 @@ public class License_main extends AppCompatActivity {
                             }
                         }
                         if (!isExist){
-                            password = getPassword(str);
+                            password = getDeltaDate(reverseStr(getPassword1(str.substring(str.length() - 6))));
                             textView.setText("授权码为: " + password + "(长按复制)");
                             licenses license = new licenses();
                             license.setImei(str);
@@ -210,7 +277,7 @@ public class License_main extends AppCompatActivity {
                             }
                         }
                         if (!isExist){
-                            password = getPassword(str);
+                            password = getDeltaDate(reverseStr(getPassword1(str.substring(str.length() - 6))));
                             textView.setText("授权码为: " + password + "(长按复制)");
                             licenses license = new licenses();
                             license.setImei(str);
