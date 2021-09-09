53b25f4e9a7bd1dd709fada49eff96f0ae78fb46
Zhengyang <542865759@qq.com>
2021-09-09 09:17:00 +0800

    临沧定制License

--- app/src/main/java/com/android/license/PasswordUtil.java
+++ app/src/main/java/com/android/license/PasswordUtil.java
@@ -25,6 +25,82 @@ public class PasswordUtil {
 
     public static String getDeltaDate(String password, int deltaTime){
         if (deltaTime == 7){
+            password = password + "z";
+        }else if (deltaTime == 180){
+            password = password + "x";
+        }else if (deltaTime == 366){
+            password = password + "c";
+        }else if (deltaTime == 30){
+            password = password + "v";
+        }else if (deltaTime == 90){
+            password = password + "b";
+        }else if (deltaTime == 3660){
+            password = password + "n";
+        }
+        return password;
+    }
+
+    //编码
+    public static String encryptionForAndroid10(String password){
+        password = password.replace("0", "q");
+        password = password.replace("1", "w");
+        password = password.replace("2", "e");
+        password = password.replace("3", "r");
+        password = password.replace("4", "t");
+        password = password.replace("5", "y");
+        password = password.replace("6", "u");
+        password = password.replace("7", "i");
+        password = password.replace("8", "o");
+        password = password.replace("9", "p");
+        password = password.replace("a", "a");
+        password = password.replace("b", "s");
+        password = password.replace("c", "d");
+        password = password.replace("d", "f");
+        password = password.replace("e", "g");
+        password = password.replace("f", "h");
+
+        return password;
+    }
+
+    //编码
+    public static String encryption(String password){
+        password = password.replace("0", "q");
+        password = password.replace("1", "w");
+        password = password.replace("2", "e");
+        password = password.replace("3", "r");
+        password = password.replace("4", "t");
+        password = password.replace("5", "y");
+        password = password.replace("6", "u");
+        password = password.replace("7", "i");
+        password = password.replace("8", "o");
+        password = password.replace("9", "p");
+        password = password.replace("A", "a");
+        password = password.replace("B", "s");
+        password = password.replace("C", "d");
+        password = password.replace("D", "f");
+        password = password.replace("E", "g");
+        password = password.replace("F", "h");
+
+        return password;
+    }
+
+    //编码
+    public static String encryption1(String password){
+        password = password.replace("0", "q");
+        password = password.replace("1", "w");
+        password = password.replace("2", "e");
+        password = password.replace("3", "r");
+        password = password.replace("4", "t");
+        password = password.replace("5", "y");
+        password = password.replace("6", "u");
+        password = password.replace("7", "i");
+        password = password.replace("8", "o");
+        password = password.replace("9", "p");
+        return password;
+    }
+
+    public static String getDeltaDatex(String password, int deltaTime){
+        if (deltaTime == 7){
             password = password + "x";
         }else if (deltaTime == 180){
             password = password + "X";
@@ -41,7 +117,7 @@ public class PasswordUtil {
     }
 
     //编码
-    public static String encryptionForAndroid10(String password){
+    public static String encryptionForAndroid10x(String password){
         password = password.replace("0", "q");
         password = password.replace("1", "R");
         password = password.replace("2", "V");
@@ -63,7 +139,7 @@ public class PasswordUtil {
     }
 
     //编码
-    public static String encryption(String password){
+    public static String encryptionx(String password){
         password = password.replace("0", "q");
         password = password.replace("1", "R");
         password = password.replace("2", "V");
@@ -85,7 +161,7 @@ public class PasswordUtil {
     }
 
     //编码
-    public static String encryption1(String password){
+    public static String encryption1x(String password){
         password = password.replace("0", "a");
         password = password.replace("1", "Z");
         password = password.replace("2", "o");
